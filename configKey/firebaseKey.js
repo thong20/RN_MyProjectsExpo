3a21e9c23c89bc318c9b53d749b925fc60d169c4
thong20 <minhphong1515@gmail.com>
2021-01-12 12:48:34 +0700

    20210112-124822

---
+++ configKey/firebaseKey.js
@@ -0,0 +1,9 @@
+export default {
+  apiKey: "AIzaSyDty36PlWFNwERpm8fMC5ZFj7tAT_sQIYc",
+  authDomain: "user-management-app-25789.firebaseapp.com",
+  projectId: "user-management-app-25789",
+  storageBucket: "user-management-app-25789.appspot.com",
+  messagingSenderId: "881152772848",
+  appId: "1:881152772848:web:eda04cd64bcf1e35d1d035",
+  measurementId: "G-DD0ZKH9EN2"
+}
