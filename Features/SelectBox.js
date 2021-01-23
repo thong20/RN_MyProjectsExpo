712d26af144db2fb2856e56ccd20a8fa01cb0656
Minh Phong <minhphong1515@gmail.com>
2021-01-24 02:04:59 +0700

    Update SelectBox.js

--- Features/SelectBox.js
+++ Features/SelectBox.js
@@ -56,6 +56,10 @@ export default function SelectBox(props) {
   const [selected, setSelected] = useState({})
 
   const _onClick = (itemSelected) => {
+    if(itemSelected.price === selected.price){
+      setSelected({})
+      return
+    }
     setSelected({price: itemSelected.price})
   }
   
